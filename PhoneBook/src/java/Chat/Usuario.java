d843ffff0fb4daeb36f8bebf841b118def6d82b3
TheMarlon1993 <ac-marlon@hotmail.com>
2015-04-06 20:30:17 -0500

    fin

---
+++ PhoneBook/src/java/Chat/Usuario.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Chat;
+import java.util.ArrayList;
+/**
+ *
+ * @author aC-Ma_000
+ */
+
+public class Usuario { 
+    
+    private static ArrayList  ArrayUsuarios = new ArrayList ();
+
+    public ArrayList getUsuarios () {
+        return ArrayUsuarios;
+    }
+
+    public void addUsuarios(String usuario) {
+        this.ArrayUsuarios.add(usuario);
+    }
+    public void deleteUsuario(String usuario){
+        this.ArrayUsuarios.remove(usuario);
+    }
+    
+}
